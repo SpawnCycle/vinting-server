30e2ae572abe93da7bd40d0d26925d02d20353fb
SpawnCycle <joloman386@gmail.com>
2026-03-16 19:29:52 +0100

    role seed migration added

---
+++ migrations/src/m20260316_seed_roles.rs
@@ -0,0 +1,84 @@
+use entity::role;
+use sea_orm::{
+    ActiveModelTrait, ActiveValue::Set, ColumnTrait, EntityTrait, QueryFilter,
+    sea_query::prelude::Utc,
+};
+use sea_orm_migration::prelude::*;
+
+const ROLE_NAMES: [&str; 2] = ["Admin", "User"];
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        let now = Utc::now().naive_local();
+
+        for role in ROLE_NAMES {
+            role::ActiveModel {
+                created_at: Set(now),
+                modified_at: Set(now),
+                deleted_at: Set(None),
+                name: Set(role.to_string()),
+                ..Default::default()
+            }
+            .insert(db)
+            .await?;
+        }
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+
+        role::Entity::delete_many()
+            .filter(role::Column::Name.is_in(ROLE_NAMES))
+            .exec(db)
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use entity::role;
+    use sea_orm::{Database, DbConn, DbErr};
+    use sea_orm_migration::{MigrationTrait, SchemaManager};
+
+    async fn prepare_db() -> Result<DbConn, DbErr> {
+        let db = Database::connect("sqlite::memory:").await?;
+        db.get_schema_registry("entity::*").sync(&db).await?;
+        Ok(db)
+    }
+
+    #[tokio::test]
+    async fn can_run_migration() -> anyhow::Result<()> {
+        let db = prepare_db().await?;
+
+        let manager = SchemaManager::new(&db);
+        super::Migration.up(&manager).await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn migration_created_all_items() -> anyhow::Result<()> {
+        let db = prepare_db().await?;
+
+        let manager = SchemaManager::new(&db);
+        super::Migration.up(&manager).await?;
+
+        for name in super::ROLE_NAMES {
+            let _role = role::Entity::find_by_name(name)
+                .one(&db)
+                .await?
+                .ok_or(anyhow::anyhow!("Could not find a role in the db"))?;
+        }
+
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -13,5 +13,3 @@ pub mod jwt;
 pub mod responder;
 pub mod routable_file_server;
 pub mod routes;
-
-pub use entity; // for schema registry
